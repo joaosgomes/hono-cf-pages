18ea67bfdd78b05699ed5737f262b436163bff66
joaosgomes <126303916+joaosgomes@users.noreply.github.com>
2025-03-25 21:36:52 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -66,6 +66,9 @@ app.get('/text', (c) => c.json('Hello Cloudflare Workers!'))
 app.get('/weak-etag/commercialsearch-com-news/economists-view-distress-opportunity-awaits-or-does-it/', (c) => {
   return c.text('W/"52b695334001ee0701d7a0f4bafb015a"', {
     headers: { 
+      'content-type': 'text/html; charset=UTF-8',
+      'vary': 'Accept-Encoding',
+      'accept-ranges': 'bytes',
       'etag': 'W/"52b695334001ee0701d7a0f4bafb015a"',
       'content-encoding': 'gzip',
       'cache-control': 'no-cache, must-revalidate, max-age=0, no-store',
