6531516ca04c563b6372bb3c54b13111dd2eea29
joaosgomes <126303916+joaosgomes@users.noreply.github.com>
2025-07-09 10:15:39 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -71,7 +71,7 @@ app.get('/error-500.html', (c) => {
   <div>::CLOUDFLARE_ERROR_500S_BOX::</div>
 
   <p>
-    500 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    500 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
